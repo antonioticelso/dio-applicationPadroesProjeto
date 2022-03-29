ab769dc8acdc687b8c2db125e00143f29989991a
prado <antonio.ti.celso@gmail.com>
2022-03-29 23:45:30 +0000

    Criaçao de estruturas de Padroes de Projetos Arquiteturais

---
+++ src/test/java/com/github/antonioticelso/api/dioapplicationPadroesProjeto/DioApplicationPadroesProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.antonioticelso.api.dioapplicationPadroesProjeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApplicationPadroesProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
