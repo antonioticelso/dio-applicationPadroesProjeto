0939cd8af9d3a906c9762fa1ab77831cd4f614da
prado <antonio.ti.celso@gmail.com>
2022-03-30 23:17:39 +0000

    Incluindo Javadoc

--- src/main/java/com/github/antonioticelso/api/dioapplicationPadroesProjeto/DioApplicationPadroesProjetoApplication.java
+++ src/main/java/com/github/antonioticelso/api/dioapplicationPadroesProjeto/DioApplicationPadroesProjetoApplication.java
@@ -4,6 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @author prado
+ *
+ * Projeto Spring Boot
+ * Modelos adotados:
+ * Spring Data JPA
+ * Spring Web
+ * H2
+ * OpenFeing
+ *
+ */
+
 @EnableFeignClients
 @SpringBootApplication
 public class DioApplicationPadroesProjetoApplication {
