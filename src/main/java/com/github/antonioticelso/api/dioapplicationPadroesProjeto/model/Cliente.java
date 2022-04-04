3d1b1a404b272c4c39c3c51fd4565ea7aa3dc5ba
prado <antonio.ti.celso@gmail.com>
2022-04-04 01:04:20 +0000

    Service - Criando service

--- src/main/java/com/github/antonioticelso/api/dioapplicationPadroesProjeto/model/Cliente.java
+++ src/main/java/com/github/antonioticelso/api/dioapplicationPadroesProjeto/model/Cliente.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -20,5 +18,20 @@ public class Cliente {
     private Long id;
 
     private String nome;
+
+    @ManyToOne
     private Endereco endereco;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id) && Objects.equals(nome, cliente.nome) && Objects.equals(endereco, cliente.endereco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, endereco);
+    }
 }
